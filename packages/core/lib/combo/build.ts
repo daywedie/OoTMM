65fb233de9cef6604f5db7f7dd7a1ec82dd661bb
daywedie <62147608+daywedie@users.noreply.github.com>
2026-08-21 06:58:54 -0300

    vai

--- packages/core/lib/combo/build.ts
+++ packages/core/lib/combo/build.ts
@@ -27,11 +27,38 @@ const cloneDependencies = async () => {
 #define _STDDEF_H_
 
 typedef unsigned int size_t;
-typedef long ptrdiff_t;
+typedef int ptrdiff_t;
 #define NULL ((void*)0)
 #define offsetof(type, member) __builtin_offsetof(type, member)
 
 #endif /* _STDDEF_H_ */
+`);
+
+      await fs.promises.writeFile(path.resolve(includeDir, 'stdint.h'), `#ifndef _STDINT_H_
+#define _STDINT_H_
+
+typedef signed char int8_t;
+typedef unsigned char uint8_t;
+typedef signed short int16_t;
+typedef unsigned short uint16_t;
+typedef signed int int32_t;
+typedef unsigned int uint32_t;
+typedef signed long long int64_t;
+typedef unsigned long long uint64_t;
+
+typedef int8_t int_least8_t;
+typedef uint8_t uint_least8_t;
+typedef int16_t int_least16_t;
+typedef uint16_t uint_least16_t;
+typedef int32_t int_least32_t;
+typedef uint32_t uint_least32_t;
+typedef int64_t int_least64_t;
+typedef uint64_t uint_least64_t;
+
+typedef int32_t intptr_t;
+typedef uint32_t uintptr_t;
+
+#endif /* _STDINT_H_ */
 `);
 
       await fs.promises.writeFile(path.resolve(includeDir, 'string.h'), `#ifndef _STRING_H_
